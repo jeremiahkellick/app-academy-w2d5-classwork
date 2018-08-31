a0eb2cbb4316f951064f96e8a0c595d1982525f4
jeremiahkellick <jeremiah@jeremiahkellick.com>
2018-08-31 16:54:28 -0700

    Create DynamicArray

--- hash-map-and-lru-cache/lib/p07_dynamic_array_bonus.rb
+++ hash-map-and-lru-cache/lib/p07_dynamic_array_bonus.rb
@@ -25,6 +25,8 @@ class StaticArray
 end
 
 class DynamicArray
+  include Enumerable
+
   attr_reader :count
 
   def initialize(capacity = 8)
@@ -33,37 +35,68 @@ class DynamicArray
   end
 
   def [](i)
+    i = @count + i if i < 0
+    @store[i]
+  rescue => e
+    if e.message == "Overflow error"
+      return nil
+    else
+      raise
+    end
   end
 
   def []=(i, val)
+    i = @count + i if i < 0
+    resize! while i >= @store.length
+    @store[i] = val
   end
 
   def capacity
     @store.length
   end
 
-  def include?(val)
-  end
-
   def push(val)
+    resize! if @count == @store.length
+    @store[@count] = val
+    @count += 1
   end
 
   def unshift(val)
+    resize! if @count == @store.length
+    (@store.length - 1).downto(1).each do |i|
+      @store[i] = @store[i - 1]
+    end
+    @store[0] = val
   end
 
   def pop
+    return nil if @count == 0
+    popped = @store[@count - 1]
+    @store[@count - 1] = nil
+    @count -= 1
+    popped
   end
 
   def shift
+    shifted = @store[0]
+    (0..@store.length - 2).each do |i|
+      @store[i] = @store[i + 1]
+    end
+    @count -= 1
+    shifted
   end
 
   def first
+    @store[0]
   end
 
   def last
+    @store[@count - 1]
   end
 
-  def each
+  def each(&prc)
+    (0...@count).each { |i| prc.call(@store[i]) }
+    self
   end
 
   def to_s
@@ -72,7 +105,8 @@ class DynamicArray
 
   def ==(other)
     return false unless [Array, DynamicArray].include?(other.class)
-    # ...
+    each_with_index { |el, i| return false unless other[i] == el }
+    true
   end
 
   alias_method :<<, :push
@@ -81,5 +115,8 @@ class DynamicArray
   private
 
   def resize!
+    new_store = StaticArray.new(@store.length * 2)
+    (0...@store.length).each { |i| new_store[i] = @store[i] }
+    @store = new_store
   end
 end
