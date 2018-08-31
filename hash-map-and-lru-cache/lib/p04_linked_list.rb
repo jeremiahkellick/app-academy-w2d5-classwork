b4ff5b20c0c7950604fc586b217565b535a9c4fe
jeremiahkellick <jeremiah@jeremiahkellick.com>
2018-08-31 14:46:06 -0700

    Create LinkedList

--- hash-map-and-lru-cache/lib/p04_linked_list.rb
+++ hash-map-and-lru-cache/lib/p04_linked_list.rb
@@ -19,7 +19,12 @@ class Node
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head, @tail = Node.new, Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -28,34 +33,72 @@ class LinkedList
   end
 
   def first
+    if @head.next == @tail
+      return nil
+    else
+      return @head.next
+    end
   end
 
   def last
+    if @tail.prev == @head
+      return nil
+    else
+      return @tail.prev
+    end
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    self.each do |node|
+      return node.val if node.key == key
+    end
+    nil
   end
 
   def include?(key)
+    !!self.find { |n| n.key == key }
   end
 
   def append(key, val)
+    node = Node.new(key, val)
+
+    last = @tail.prev
+    last.next = node
+    node.next = @tail
+    @tail.prev = node
+    node.prev = last
   end
 
   def update(key, val)
+    node = self.find { |n| n.key == key }
+    unless node.nil?
+      node.val = val
+    end
   end
 
   def remove(key)
+    node = self.find { |n| n.key == key }
+    unless node.nil?
+      (node.prev).next = node.next
+      (node.next).prev = node.prev
+    end
   end
 
-  def each
+  def each(&prc)
+    current = @head.next
+    until current == @tail
+      prc.call(current)
+      current = current.next
+    end
+    self
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
