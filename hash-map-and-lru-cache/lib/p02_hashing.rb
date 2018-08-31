26d14138fb61f473d3baf7ffafaacc8696b59831
jeremiahkellick <jeremiah@jeremiahkellick.com>
2018-08-31 14:45:28 -0700

    Create hash methods

--- hash-map-and-lru-cache/lib/p02_hashing.rb
+++ hash-map-and-lru-cache/lib/p02_hashing.rb
@@ -4,11 +4,19 @@ end
 
 class Array
   def hash
+    combined = 0
+    self.each_with_index { |el, i| combined = combined ^ (el + i) }
+    combined.hash
   end
 end
 
 class String
   def hash
+    combined = 0
+    self.chars.each_with_index do |char, i|
+      combined = combined ^ (char.ord + i)
+    end
+    combined.hash
   end
 end
 
@@ -16,6 +24,8 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    combined = 0
+    self.each { |key, val| combined = combined ^ (key.to_s.hash + val.hash) }
+    combined.hash
   end
 end
